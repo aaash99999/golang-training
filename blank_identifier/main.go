3ff8544ff790c8e82eb5f413a838f0b299271e11
aaash99999 <aaash99999@gmail.com>
2016-11-20 20:40:38 +1100

    blank identifier

---
+++ blank_identifier/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	res, _ := http.Get("http://www.google.com/")
+	defer res.Body.Close()
+	page, _ := ioutil.ReadAll(res.Body)
+	fmt.Printf("%s", page)
+}
