ba112f3aaad649c535cb578b1dd0a6385a9b4a48
aaash99999 <aaash99999@gmail.com>
2016-11-27 22:51:08 +1100

    code cleanup

--- robots/main.go
+++ robots/main.go
@@ -1,3 +1,6 @@
+/*
+Searches a URL for a given regular expression
+*/
 package main
 
 import (
@@ -12,40 +15,40 @@ import (
 
 func main() {
 	basename := filepath.Base(os.Args[0])
-	site := os.Args[1:] // don't include program name
-	if len(site) != 2 {
+	arguments := os.Args[1:] // [1:] omits the program name, as we only want the args
+
+	if len(arguments) != 2 {
 		fmt.Printf("Usage: %s <URL> <regex to search for>\n", basename)
 		os.Exit(1)
 	}
-	// site[0] is the URL to check
-	// site[1] is the regex to search for
 
-	searchstring := site[1]
+	searchURL := arguments[0]
+	searchString := arguments[1]
 
-	res, err := http.Get(site[0])
+	res, err := http.Get(searchURL)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	pagebody, err := ioutil.ReadAll(res.Body)
+	pageBody, err := ioutil.ReadAll(res.Body)
 	res.Body.Close()
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Searching %s for '%s'\n", site[0], searchstring)
-	matched, err := regexp.Match(searchstring, pagebody)
+	fmt.Printf("Searching %s for '%s'\n", searchURL, searchString)
+	matched, err := regexp.Match(searchString, pageBody)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	if matched {
-		fmt.Printf("Success! Found: '%s'\n", searchstring)
+		fmt.Printf("Success! Found: '%s'\n", searchString)
 	} else {
-		fmt.Printf("Fail! Did not find: '%s'\n", searchstring)
+		fmt.Printf("Fail! Did not find: '%s'\n", searchString)
 	}
 
 }
